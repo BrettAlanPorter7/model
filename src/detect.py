03e78b39e9fa48b2fead547dc68da7fb63f4648a
BrettAlanPorter7 <166972319+BrettAlanPorter7@users.noreply.github.com>
2025-04-02 10:50:07 -0500

    Update detect.py
    Attempt to input MavProxy communication to Mission Planner

--- src/detect.py
+++ src/detect.py
@@ -3,6 +3,7 @@ from .models import ModelInterface
 import cv2
 import time
 from mjpeg_streamer import Stream, MjpegServer
+from pymavlink import mavutil
 
 
 def object_detection_loop(camera: CameraInterface, model: ModelInterface):
@@ -15,6 +16,11 @@ def object_detection_loop(camera: CameraInterface, model: ModelInterface):
     server = MjpegServer("0.0.0.0", 8080)
     server.add_stream(stream)
     server.start()
+
+    print("Connecting to drone...")
+    master = mavutil.mavlink_connection('udp:127.0.0.1:14550')  #adjust for actual drone connection
+    master.wait_heartbeat()  #make sure it is connected
+    print("✅ Drone connected!")
 
     while True:
         image = run_object_detection(camera, model)
@@ -48,9 +54,47 @@ def object_detection_loop(camera: CameraInterface, model: ModelInterface):
 
 
 def run_object_detection(camera: CameraInterface, model: ModelInterface):
-    array, image = camera.capture()
+    array, image = capture() #pulls array, image from capture
+	results = model.detect(array, image) #potentially need to fix this so we get the person detection, maybe results = (array)?
 
-    results = model.detect(array, image)
+	no_person_detected = all(res.name != "person" for res in results)
+
+	msg = master.recv_match(type='HEARTBEAT', blocking=True) #pulls heartbeat message
+	mode = mavutil.mode_string_v10(msg) # pulls flight mode from heartbeat message
+	print(f"Current flight mode: {mode}")
+
+	if no_person_detected: 
+		if mode != 'LOITER':
+			#print("✅ No person detected") removed so this is not constantly printed, is continue the correct thing to put next?
+			continue
+
+		else: #meaning the drone is currently in loiter mode
+			print("✅ No person detected")
+			#change mode to auto because no person detected
+			master.mav.command_long_send(
+				master.target_system, master.target_component,
+				mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+				0,  # Base mode (ignored)
+				mavutil.mavlink.enums['MAV_MODE_AUTO_DISARMED'].value,
+				0, 0, 0, 0, 0  # Unused parameters
+			)
+
+	else: # person is detected
+		if mode != 'LOITER':
+			print("🚨 Human detected! Halting drone for 5 seconds... 🚨")
+			#change mode to loiter because person detected
+			master.mav.command_long_send(
+				master.target_system, master.target_component,
+				mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+				0,  # Base mode (ignored)
+				mavutil.mavlink.enums['MAV_MODE_LOITER'].value,
+				0, 0, 0, 0, 0  # Unused parameters
+			)
+			time.sleep(5) #wait 5 seconds
+
+		else: #drone already in loiter mode
+			print("🚨 Human detected! Halting drone for 5 seconds... 🚨")
+			time.sleep(5) #wait 5 seconds
 
     for result in results:
         # Detection box
